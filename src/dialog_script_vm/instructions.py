0985d6ff3b7c35446546b75d2a297adeb8c0122f
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-05-22 16:31:39 -0600

    VM: Add instructions for loading/saving variables.

--- src/dialog_script_vm/instructions.py
+++ src/dialog_script_vm/instructions.py
@@ -67,6 +67,52 @@ class DialogScriptNop(DialogScriptInstruction):
         pass
 
 
+class DialogScriptPushLiteral(DialogScriptInstruction):
+    def __init__(self, literal: TLiteral):
+        self._literal = literal
+
+    def execute(self, prog: DialogScriptVMContext):
+        prog.push(self._literal)
+
+
+class DialogScriptLoadVar(DialogScriptInstruction):
+    """
+    Load a variable to the stack
+    """
+    def __init__(self, var: str):
+        self._var = var
+
+    def execute(self, prog: DialogScriptVMContext):
+        val = prog.load_var(self._var)
+        prog.push(val)
+
+
+class DialogScriptWriteVar(DialogScriptInstruction):
+    """
+    Load a variable to the stack
+    """
+    def __init__(self, var: str):
+        self._var = var
+
+    def execute(self, prog: DialogScriptVMContext):
+        val = prog.pop()
+        prog.save_var(self._var, val)
+
+class DialogScriptPop(DialogScriptInstruction):
+    def execute(self, prog: DialogScriptVMContext):
+        prog.mem_stack.pop()
+
+
+class DialogScriptPopMulti(DialogScriptInstruction):
+    def __init__(self, count: int):
+        if count <= 0:
+            raise RuntimeError('Must pop a positive amount')
+        self.count = count
+
+    def execute(self, prog: DialogScriptVMContext):
+        del prog.mem_stack[-self.count:]
+
+
 class DialogScriptOptInst(DialogScriptInstruction):
     def __init__(self, options: ty.List[DialogOption]):
         self.options = options
