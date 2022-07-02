d361ad7437647c4c90c238986e032d49d1d9917c
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-07-02 02:36:31 -0600

    TEST: Add remaining and/or.

--- tests/test_ds_binaryop.py
+++ tests/test_ds_binaryop.py
@@ -16,3 +16,66 @@ def test_ds_equality():
     gvars = vm.context.global_vars
     assert gvars['var1'] is False
     assert gvars['var2'] is True
+
+
+def test_ds_andop():
+    program_text = """
+    let var1;
+    let var2;
+    let var3;
+    let var4;
+    
+    begin block start
+        var1 = 100 and false;
+        var2 = true and 25;
+        var3 = false && true;
+        var4 = true && true;
+    end;
+    """
+
+    vm = exec_from_text(program_text)
+    gvars = vm.context.global_vars
+    assert gvars['var1'] is False
+    assert gvars['var2'] is 25
+    assert gvars['var3'] is False
+    assert gvars['var4'] is True
+
+
+def test_ds_orop():
+    program_text = """
+    let var1;
+    let var2;
+    let var3;
+    let var4;
+    
+    begin block start
+        var1 = 100 or false;
+        var2 = 0 or 0;
+        var3 = false || false;
+        var4 = true || false;
+    end;
+    """
+
+    vm = exec_from_text(program_text)
+    gvars = vm.context.global_vars
+    assert gvars['var1'] is 100
+    assert gvars['var2'] is 0
+    assert gvars['var3'] is False
+    assert gvars['var4'] is True
+
+
+def test_ds_parentheses():
+    program_text = """
+    let var1;
+    let var2;
+    
+    begin block start
+        var1 = (false && false) || true;
+        var2 = false && (false || true);
+    end;
+    """
+
+    vm = exec_from_text(program_text)
+    gvars = vm.context.global_vars
+    assert gvars['var1'] is True
+    assert gvars['var2'] is False
