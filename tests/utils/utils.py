de881955af1ced677fc79b3c368242a55cd4478d
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-07-02 02:34:49 -0600

    TESTUTIL: Add exec_from_text function to utils.

--- tests/utils/utils.py
+++ tests/utils/utils.py
@@ -1,8 +1,18 @@
 import os
 
+from dialog_script_vm.vm import DialogScriptVM
+from instgen.ds_compiler import program_from_text
+
 TEST_RESOURCES_DIR = 'test_resources'
 
 
 def from_test_resources(filename: str) -> os.PathLike:
     # noinspection PyTypeChecker
     return os.path.join(TEST_RESOURCES_DIR, filename)
+
+
+def exec_from_text(text: str) -> DialogScriptVM:
+    prog, _ = program_from_text(text)
+    vm = DialogScriptVM(prog)
+    vm.execute()
+    return vm
